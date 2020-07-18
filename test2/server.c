97538d6c00c9ce1fee8fac7f7f5ce8ba56112a59
lancibe <1609547089@qq.com>
2020-07-18 18:04:15 +0800

    first

---
+++ test2/server.c
@@ -0,0 +1,63 @@
+// 基于C/S模型的服务端
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <errno.h>
+
+#include "/usr/local/mysql/include/mysql.h"
+
+
+
+//自定义错误处理函数
+void my_err(const char* err_string, int line)
+{
+	fprintf(stderr, "line:%d  ", line);
+	perror(err_string);
+	exit(1);
+}
+
+
+
+//调用数据库
+void BindMysql(void)
+{
+    MYSQL mysql;
+    //初始化一个句柄
+    if(NULL == mysql_init(&mysql))
+    {
+		my_err("mysql_init", __LINE__);
+	}
+
+	//初始化数据库
+	if(mysql_library_init(0, NULL, NULL) != 0)
+    {
+		my_err("mysql_library_init", __LINE__);
+	}
+
+	//连接数据库
+	if(NULL == mysql_real_connect(&mysql, "127.0.0.1", "root", "Zyx20010326zyx", "users", 0, NULL, 0))
+    {
+		my_err("mysql_real_connect", __LINE__);
+	}
+
+	//设置中文字符集
+	if(mysql_set_character_set(&mysql, "utf8") < 0)
+    {
+		my_err("mysql_set_character_set", __LINE__);
+	}
+}    
+
+
+
+
+int main(void)
+{
+    BindMysql();
+    return 0;
+}
